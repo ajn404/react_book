0c0a48c9a5dd0559f78a1ed4472f75425c6ab922
n-graymoon <huiyue.ning@71360.com>
2021-10-29 16:59:35 +0800

    no message

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Example from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Example />
   </React.StrictMode>,
   document.getElementById('root')
 );
